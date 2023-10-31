b9559924c21604be21aaf8b7769fba418261341d
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2023-10-31 15:48:37 +0300

    images adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 49, 2, 74),
+      backgroundColor: const Color.fromARGB(255, 49, 2, 74),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
+            Padding(
+              padding: EdgeInsets.only(bottom: 32.0),
+              child: Image.asset(
+                "assets/ozge_akinci.jpg",
+                width: 300,
+                height: 300,
+              ),
+            ),
+            const Text(
               'Özge Akıncı',
               style: TextStyle(fontSize: 35, color: Colors.white),
             ),
-            Text(
+            const Text(
               'Tobeto-Mobil Geliştirici 1A',
               style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-            Text(
+            const Text(
               '31.10.2023',
               style: TextStyle(fontSize: 25, color: Colors.white),
             ),
