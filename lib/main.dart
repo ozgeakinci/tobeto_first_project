61d03c5176d8e79622b70edd6e77a51f96e03fbd
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2023-10-31 16:29:01 +0300

    görsel olmadandan önceki hali

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       backgroundColor: const Color.fromARGB(255, 49, 2, 74),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 32.0),
-              child: Image.asset(
-                "assets/ozge_akinci.jpg",
-                width: 300,
-                height: 300,
-              ),
-            ),
-            const Text(
+            // Padding(
+            //   padding: EdgeInsets.only(bottom: 32.0),
+            //   child: Image.asset(
+            //     "assets/ozge_akinci.jpg",
+            //     width: 300,
+            //     height: 300,
+            //   ),
+            // ),
+            Text(
               'Özge Akıncı',
               style: TextStyle(fontSize: 35, color: Colors.white),
             ),
-            const Text(
+            Text(
               'Tobeto-Mobil Geliştirici 1A',
               style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-            const Text(
+            Text(
               '31.10.2023',
               style: TextStyle(fontSize: 25, color: Colors.white),
             ),
